7e30f782f377e5e391f3e11a254d51c652596036
chaipat supharak <chaipat.sup@cpf.co.th>
2023-03-27 09:51:37 +0700

    timeline

--- lib/main.dart
+++ lib/main.dart
@@ -28,42 +28,170 @@ class TimeLineWidget extends StatelessWidget {
       appBar: AppBar(
         title: Text("Timeline"),
       ),
-      body: Column(children: [
-        TimelineTile(
-          alignment: TimelineAlign.center,
-          startChild: Container(
-            constraints: const BoxConstraints(minHeight: 120),
-            color: Colors.pink,
-          ),
-          endChild: Container(
-            constraints: const BoxConstraints(minHeight: 120),
-            color: Colors.blue.shade400,
-          ),
-          indicatorStyle: IndicatorStyle(
-              width: 40,
-              color: Colors.amberAccent,
-              padding: const EdgeInsets.all(8),
-              iconStyle: IconStyle(
-                  iconData: Icons.insert_emoticon, color: Colors.black)),
-        ),
-        TimelineTile(
-          alignment: TimelineAlign.center,
-          startChild: Container(
-            constraints: const BoxConstraints(minHeight: 120),
-            color: Colors.pink,
-          ),
-          endChild: Container(
-            constraints: const BoxConstraints(minHeight: 120),
-            color: Colors.blue.shade400,
-          ),
-          indicatorStyle: IndicatorStyle(
-              width: 40,
-              color: Colors.amberAccent,
-              padding: const EdgeInsets.all(8),
-              iconStyle: IconStyle(
-                  iconData: Icons.insert_emoticon, color: Colors.black)),
-        )
-      ]),
+      body: SingleChildScrollView(
+        child: TimeLine(),
+      ),
     );
+  }
+}
+
+class TimeLine extends StatelessWidget {
+  const TimeLine({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+      TimelineTile(
+        isFirst: true,
+        alignment: TimelineAlign.center,
+        startChild: Container(
+          constraints: const BoxConstraints(minHeight: 120),
+          color: Colors.pink,
+        ),
+        endChild: Container(
+          constraints: const BoxConstraints(minHeight: 120),
+          color: Colors.blue.shade400,
+        ),
+        indicatorStyle: IndicatorStyle(
+            width: 40,
+            color: Colors.amberAccent,
+            padding: const EdgeInsets.all(8),
+            iconStyle: IconStyle(
+                iconData: Icons.insert_emoticon, color: Colors.black)),
+      ),
+      TimelineTile(
+        alignment: TimelineAlign.center,
+        startChild: Container(
+          constraints: const BoxConstraints(minHeight: 120),
+          color: Colors.pink,
+        ),
+        endChild: Container(
+          constraints: const BoxConstraints(minHeight: 120),
+          color: Colors.blue.shade400,
+        ),
+        indicatorStyle: IndicatorStyle(
+            width: 40,
+            color: Colors.amberAccent,
+            padding: const EdgeInsets.all(8),
+            iconStyle: IconStyle(
+                iconData: Icons.insert_emoticon, color: Colors.black)),
+      ),
+      TimelineTile(
+        alignment: TimelineAlign.manual,
+        lineXY: 0.3, //0.5 ไปทางขวา 50%  //0.4 ไปทางขวา 40%
+        startChild: Container(
+          constraints: const BoxConstraints(minHeight: 300),
+          // color: Colors.pink,
+        ),
+        endChild: Container(
+          constraints: const BoxConstraints(minHeight: 300),
+          // color: Colors.blue.shade400,
+          child: Center(child: Text("Hello")),
+        ),
+        indicatorStyle: IndicatorStyle(
+            width: 40,
+            color: Colors.amberAccent,
+            padding: const EdgeInsets.all(8),
+            iconStyle: IconStyle(
+                iconData: Icons.insert_emoticon, color: Colors.black)),
+      ),
+       TimelineTile(
+        alignment: TimelineAlign.manual,
+        lineXY: 0.3, //0.5 ไปทางขวา 50%  //0.4 ไปทางขวา 40%
+        startChild: Container(
+          constraints: const BoxConstraints(minHeight: 300),
+          // color: Colors.pink,
+        ),
+        endChild: Container(
+          constraints: const BoxConstraints(minHeight: 300),
+          // color: Colors.blue.shade400,
+          child: Center(child: Text("Hello")),
+        ),
+        indicatorStyle: IndicatorStyle(
+            width: 40,
+            color: Colors.amberAccent,
+            padding: const EdgeInsets.all(8),
+            iconStyle: IconStyle(
+                iconData: Icons.insert_emoticon, color: Colors.black)),
+      ),
+       TimelineTile(
+        alignment: TimelineAlign.manual,
+        lineXY: 0.3, //0.5 ไปทางขวา 50%  //0.4 ไปทางขวา 40%
+        startChild: Container(
+          constraints: const BoxConstraints(minHeight: 300),
+          // color: Colors.pink,
+        ),
+        endChild: Container(
+          constraints: const BoxConstraints(minHeight: 300),
+          // color: Colors.blue.shade400,
+          child: Center(child: Text("Hello")),
+        ),
+        indicatorStyle: IndicatorStyle(
+            width: 40,
+            color: Colors.amberAccent,
+            padding: const EdgeInsets.all(8),
+            iconStyle: IconStyle(
+                iconData: Icons.insert_emoticon, color: Colors.black)),
+      ),
+       TimelineTile(
+        alignment: TimelineAlign.manual,
+        lineXY: 0.3, //0.5 ไปทางขวา 50%  //0.4 ไปทางขวา 40%
+        startChild: Container(
+          constraints: const BoxConstraints(minHeight: 300),
+          // color: Colors.pink,
+        ),
+        endChild: Container(
+          constraints: const BoxConstraints(minHeight: 300),
+          // color: Colors.blue.shade400,
+          child: Center(child: Text("Hello")),
+        ),
+        indicatorStyle: IndicatorStyle(
+            width: 40,
+            color: Colors.amberAccent,
+            padding: const EdgeInsets.all(8),
+            iconStyle: IconStyle(
+                iconData: Icons.insert_emoticon, color: Colors.black)),
+      ),
+       TimelineTile(
+        alignment: TimelineAlign.manual,
+        lineXY: 0.3, //0.5 ไปทางขวา 50%  //0.4 ไปทางขวา 40%
+        startChild: Container(
+          constraints: const BoxConstraints(minHeight: 300),
+          // color: Colors.pink,
+        ),
+        endChild: Container(
+          constraints: const BoxConstraints(minHeight: 300),
+          // color: Colors.blue.shade400,
+          child: Center(child: Text("Hello")),
+        ),
+        indicatorStyle: IndicatorStyle(
+            width: 40,
+            color: Colors.amberAccent,
+            padding: const EdgeInsets.all(8),
+            iconStyle: IconStyle(
+                iconData: Icons.insert_emoticon, color: Colors.black)),
+      ),
+       TimelineTile(
+        alignment: TimelineAlign.manual,
+        lineXY: 0.3, //0.5 ไปทางขวา 50%  //0.4 ไปทางขวา 40%
+        startChild: Container(
+          constraints: const BoxConstraints(minHeight: 300),
+          // color: Colors.pink,
+        ),
+        endChild: Container(
+          constraints: const BoxConstraints(minHeight: 300),
+          // color: Colors.blue.shade400,
+          child: Center(child: Text("Hello")),
+        ),
+        indicatorStyle: IndicatorStyle(
+            width: 40,
+            color: Colors.amberAccent,
+            padding: const EdgeInsets.all(8),
+            iconStyle: IconStyle(
+                iconData: Icons.insert_emoticon, color: Colors.black)),
+      ),
+    ]);
   }
 }
---
+++ readme.txt
@@ -0,0 +1,4 @@
+1.การกำหนดความยาวของเส้น
+https://github.com/JHBitencourt/timeline_tile/issues/25
+
+2.
